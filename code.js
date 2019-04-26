d9c0a2d5d1696199c66668892a18d800753e33fe
IamLAM <ingeniero@luismonge.com.mx>
2019-04-26 15:52:50 -0700

    Parameters detected

--- code.js
+++ code.js
@@ -1,30 +1,48 @@
 function checkCashRegister(price, cash, cid) {
     var change;
+    let box = 0;
+    let dev = 0;
     // Here is your change, ma'am.
 
     for (var c in cid) {
 
-        if (collection[c].hasOwnProperty(pre) && Boolean(collection[c][pre])) {
-            counter++;
-        }
-        console.log(collection[c].user);
+
+        box += cid[c][1];
+        // console.log(box);
+        dev = price - cash;
+        console.log(dev);
+        console.log(cid[c][1]);
+        console.log(box);
+
     }
 
+
     return change;
-  }
-  
-  // Example cash-in-drawer array:
-  // [["PENNY", 1.01],
-  // ["NICKEL", 2.05],
-  // ["DIME", 3.1],
-  // ["QUARTER", 4.25],
-  // ["ONE", 90],
-  // ["FIVE", 55],
-  // ["TEN", 20],
-  // ["TWENTY", 60],
-  // ["ONE HUNDRED", 100]]
-  
-  checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+}
+
+
+// Example cash-in-drawer array:
+// [["PENNY", 1.01],
+// ["NICKEL", 2.05],
+// ["DIME", 3.1],
+// ["QUARTER", 4.25],
+// ["ONE", 90],
+// ["FIVE", 55],
+// ["TEN", 20],
+// ["TWENTY", 60],
+// ["ONE HUNDRED", 100]]
+
+checkCashRegister(19.5, 20, [
+    ["PENNY", 1.01],
+    ["NICKEL", 2.05],
+    ["DIME", 3.1],
+    ["QUARTER", 4.25],
+    ["ONE", 90],
+    ["FIVE", 55],
+    ["TEN", 20],
+    ["TWENTY", 60],
+    ["ONE HUNDRED", 100]
+]);
 
 /*
 
