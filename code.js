bd259b3e966ec38e0abd3bfaea8c3f2a3123fe28
IamLAM <ingeniero@luismonge.com.mx>
2019-04-27 12:37:03 -0700

    Comparing funds vs change

--- code.js
+++ code.js
@@ -14,11 +14,45 @@ function checkCashRegister(price, cash, cid) {
         if (dev > box) {
             // return {status: "INSUFFICIENT_FUNDS", change: []};
             console.log("INSUFFICIENT_FUNDS");
-        } else if (dev == box) {
-            return { status: "CLOSED", change: cid };
+        } else {
+            let i = 0;
+            //while (dev <= 0) {
+
+            if (cid[8][1] >= dev) {
+                console.log(dev + " is less than One hundread " + cid[8][1]);
+            }
+            if (cid[7][1] >= dev) {
+                console.log("is less than Twenty dollars");
+            }
+            if (cid[6][1] >= dev) {
+                console.log("is less than Ten dollars");
+            }
+            if (cid[5][1] >= dev) {
+                console.log("is smaller than Five dollars ");
+            }
+            if (cid[4][1] >= dev) {
+                console.log("is less than a Dollar");
+            }
+            if (cid[3][1] >= dev) {
+                console.log("is less than Quarter ");
+            }
+            if (cid[2][1] >= dev) {
+                console.log("is less than Dime");
+            }
+            if (cid[1][1] >= dev) {
+                console.log("is less than Nickel");
+            }
+            if (cid[0][1] >= dev) {
+                console.log("is less than Penny");
+            }
+            //}
+
         }
 
 
+    } else if (dev == box) {
+        // return { status: "CLOSED", change: cid };
+        console.log("CLOSED");
     } else {
         console.log("Sorry you need more money");
     }
@@ -41,7 +75,7 @@ function checkCashRegister(price, cash, cid) {
 // ["TWENTY", 60],
 // ["ONE HUNDRED", 100]]
 
-checkCashRegister(19.5, 2000, [
+checkCashRegister(19.5, 20, [
     ["PENNY", 1.01],
     ["NICKEL", 2.05],
     ["DIME", 3.1],
